b849ae8e2dfe067f154f0bdc5f0e820b4a1097ef
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-05 12:48:35 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import { AdNames } from "adcommon";
 import { QinPanel } from "qinpel-cps";
 
 class AdMister extends QinPanel {
   public constructor() {
     super();
     const qinDesk = this.qinpel.chief.newDesk(this.qinpel, {
-      addsApps: (manifest) => manifest.group == "admister",
-      showCfgs: false,
+      addsApps: (manifest) => manifest.group == AdNames.AdMister,
+      addsCfgs: (manifest) =>
+        [this.qinpel.our.names.QinBases as string].indexOf(manifest.title) > -1,
     });
     this.getMain().appendChild(qinDesk.getMain());
   }
